2342e7e501d79b615466e3cc25fb2f7c06d45d57
evsspranav0406 <evsspranav@gmail.com>
2025-11-01 04:30:26 +0530

    Final

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(cors({
-  origin: 'https://hd-booking-bookit-kd9f.vercel.app', // or '*' for all origins
+  origin: 'https://hd-booking-ashy.vercel.app', // or '*' for all origins
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
