36befb2d679f2ada571bfa582b88de18f76b6b15
evsspranav0406 <evsspranav@gmail.com>
2025-11-01 04:28:07 +0530

    commit

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { Experience, Booking, PromoValidation } from '../types';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = 'https://hd-booking-bookit.vercel.app';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
